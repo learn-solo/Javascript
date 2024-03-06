c0a310f5c401543f0239af2ef260f4142c5d3665
mhaemnn <muhaemin1998@gmail.com>
2024-03-06 18:04:24 +0700

    add file destructuring object & array

---
+++ #11 destructuring object &array/main.js
@@ -0,0 +1,9 @@
+const profile = {
+  firtName: "muhaemin",
+  lastName: "iskandar",
+  age: 26,
+};
+const firtName = profile.firtName;
+const lastName = profile.lastName;
+const age = profile.age;
+console.log(firtName, lastName, age);
