cd52e13a75af60a103d30357eb59072a022dbe0f
mhaemnn <muhaemin1998@gmail.com>
2024-04-25 11:37:21 +0700

    modifikasi file \#9\ array/

--- #9 array/main.js
+++ #9 array/main.js
@@ -1,41 +1,36 @@
 // array adalah tipe data yang dapat mengelompokan lebih dari satu nilai &  menepatkan nya didalam varibale
-let myArray = ["domba", 23, 45.23, true, "programming"];
-// pemanggilan array dengan index
+let myArray = ["Ibu", 12, 32.5, true, "programmer"];
 console.log(myArray[0]);
 console.log(myArray[1]);
 console.log(myArray[2]);
 console.log(myArray[3]);
 console.log(myArray[4]);
-console.log(myArray[5]);
-console.log("panjang nilai array adalah " + myArray.length + ".");
+console.log("Panjang Nilia Array " + myArray.length + ".");
 
-// manipulasi data array
-// PUSH() -> menambahkan data diakhir array
+//Manipulasi array
+// (push) menambahkan nilai diakhir array
 myArray.push(false);
 console.log(myArray);
 
-// POP() -> mengeluarkan data di akhir array
+// (pop) mengeluarkan nilai diakhir arrya
 myArray.pop();
 console.log(myArray);
 
-// UNSHIFT() -> menambahkan data di awal array
-myArray.unshift("new");
+// (unshift) menambahkan nilia diawal array
+myArray.unshift("sayang");
 console.log(myArray);
 
-// SHIFT() -> mengeluarkan data di awal array
+// (shift) mengeluarkan nilai diawal array
 myArray.shift();
 console.log(myArray);
 
-// DELETE -> menghapus data dan membiarkan posisi tersebut kosong
-delete myArray[1];
+// (delete) menghapus data pada index = membiarkan posisi tersebut kososong
+delete myArray[0];
 console.log(myArray);
 
-// SPLICE() -> menghapus data + element
-myArray.splice(2.1); //menghapus dari index 2 sebanyak 1 element
+// (splice) menghapus element
+myArray.splice(0, 1); // menghapus dari index 0, sebanyak 1 element
 console.log(myArray);
-
-// SPLICE() -> menambahkan elemen pada array dengan memberikan argument ke-3
-const week = ["minggu", "selasa", "rabu"];
-console.log("sebelum di splice", week);
-week.splice(1, 0, "senin");
-console.log("sesudah di splice", week);
+// NOTE* splice juga bisa digunakan untuk menambahkan element, dengan memberikan argument ke-3
+myArray.splice(2, 1, "baru"); //menambahkan argument selanjutnya ("baru")
+console.log(myArray);
