04669358c6093416d29e709bea33d19880f9f6c6
mhaemnn <muhaemin1998@gmail.com>
2025-06-01 20:09:52 +0700

    edit variable destructuring object array

--- #10 spread operator/main.js
+++ #10 spread operator/main.js
@@ -1,21 +1,21 @@
 // Belajar spread operator
 // Sebelum menggunakan spread operator
-const favorite = ["seafood", "salad", "nugget", "soup"];
-console.log(favorite);
+const kahayang = ["seafood", "salad", "nugget", "soup"];
+console.log(kahayang);
 
 // setelah menggunakan spread operator
-console.log(...favorite);
+console.log(...kahayang);
 
 console.log("=========================================");
 
 // Spread operator juga bisa untuk menggabungkan array
 // Sebelum
 const other = ["nasi goreng", "pizza", "keripik"];
-const allFavorite = [favorite, other];
-console.log(allFavorite);
+const allkahayang = [kahayang, other];
+console.log(allkahayang);
 
 // Sesudah
-const allOther = [...favorite, ...other];
+const allOther = [...kahayang, ...other];
 console.log(allOther);
 
 // Spread operator juga bisa digunakan untuk object literals
