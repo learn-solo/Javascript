dd085429c2c55fc8a09daf38b5b7138a98117800
mhaemnn <muhaemin1998@gmail.com>
2025-07-27 20:53:27 +0700

    update materi folder tree

---
+++ #03 variable/varibale.js
@@ -0,0 +1,21 @@
+/**
+ * varibale adalah untuk menampung sebuah nilai, ankga, teks yang menghasilkan nilai (expression)
+ */
+const id = 123;
+let username = "Dicoding";
+console.log(id + username);
+
+
+// penamaan variable yang BENAR & SALAH:
+// penamaan yang BENAR
+const firtName = "Muhaemin";
+const lest_name = "Iskandar";
+const $message = "No info";
+const userId1 = 123;
+const userId2 = 321
+
+
+// penamaan yang SALAH
+const firt-name = "salim"; //tidak boleh menggandung karakter -
+const last name = "ulala"; //tidak bole mengandung spasi
+const @message  = "No Info"; //tidak bole menggandung karakter @
