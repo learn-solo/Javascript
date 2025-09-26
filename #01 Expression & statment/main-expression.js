c22c26a4732cf718f3fcc68f19d9469c81c842ad
mhaemnn <muhaemin1998@gmail.com>
2025-09-26 13:18:15 +0700

    refactor: menyelesaikan latihan #01 Expression & statment

---
+++ #01 Expression & statment/main-expression.js
@@ -0,0 +1,19 @@
+/*
+1. Pemrograman = komunikasi dengan komputer
+  - Tujuannya: memberikan intruksi agar komputer menjalankan tugas tertentu
+2. Instruksi haru sesuai aturan
+   - Agar bisa dipahmi dan dijalankan oleh komputer
+3. Instruksi bersifat komputasional
+   - Harus jelas, terstuktur, dan sistematis
+4. Contoh instruksi
+   - Biasanya berupa perintah yang umum dijalankan oleh komputer 
+*/
+
+const age = 25;
+const name = "muhaemin";
+console.log(`aku ${name} umur saya ${age}`);
+
+const nama1 = "Dicoding"
+const nama2 = "Indonesia";
+console.log("Dicoding" + " " + "Indonesia");
+
