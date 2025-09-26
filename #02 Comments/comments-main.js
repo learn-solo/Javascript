a6269517267b4a2cd701cc8c39987746a7601fb5
mhaemnn <muhaemin1998@gmail.com>
2025-09-26 13:28:28 +0700

    feat: menambahkan folder comments

---
+++ #02 Comments/comments-main.js
@@ -0,0 +1,9 @@
+/**
+ * Komentar = cacatan tidak akan dijalankan oleh interpreter,
+ * Di Javascript, komentar bisa di tulis dengan:
+ * 
+ */
+
+// Teks ini akan di abaikan oleh interpreter
+console.log("Hai, Muhaemin"); // -> dijalankan 
+// console.log("Hai kamu") -> tidak akan di jalankan 
