96b16cbeb34baf46487ddf630fb78a41ae7052ca
mhaemnn <muhaemin1998@gmail.com>
2025-09-28 19:04:42 +0700

    menambahkan folder vaiable

---
+++ #03 Variable/variable-main.js
@@ -0,0 +1,15 @@
+//Variable adalah wadah yang menampung sebuah nilai, bisa berupa teks, angka, atau apapun yang menghasilkan (Expression)
+
+const age = 27;
+const name = "Muhaemin";
+const status = "Menikah";
+
+console.log(`saya ${name} umur ${age} sudah ${status}`);
+
+// perbedaan let dan const: let nilainya bisa di ubah sedangkan const kebalikanya.
+// sebelum di ubah
+let username = "mumu";
+console.log(username); // output: mumu
+// setelah di ubah
+username = "mhaemnn"
+console.log(username);
