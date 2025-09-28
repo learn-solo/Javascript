d3d8fe52ca678f21c477fc84987af662e5c54841
mhaemnn <muhaemin1998@gmail.com>
2025-09-28 21:07:09 +0700

    menambahkan folder vaiable

---
+++ #04 Tipe data/tipeData-main.js
@@ -0,0 +1,26 @@
+// Tipe data adalah element dasar yang sering kita disimpan dan olah untuk membuat insturksi.
+// Tipe data primitif => string, number, boolean, dan kosong (Null dan Undefined)
+
+// STRING
+let tipeData = "Ini adalah contoh tipe data string";
+console.log(tipeData);
+
+// NUMBER
+// Semua data berupa angka direpresentasikan sebagai number, baik itu bilangan bulat maupun pecahan.
+// Selain menghasilkan angka normal, number memiliki nilai special, yaitu Infinity dan Nan (Not-a-Number).
+// Nilai Infinity dihasilkan ketika melakukan operasi aritmatika yang tidak terdefinisi seperti membagi nilai dengan Nol.
+let ankga = 50 / 0;
+console.log(ankga);
+
+// Sedangkan Nan dihsilkan nilai non-numerik diubah ke tipe data number. 
+let slogan = Number ("Dicoding");
+console.log(slogan);
+
+// BOOLEAN
+// Boolean adalah tipe data yang memiliki nilai True & False.
+let angkaBoolean = 10 > 3;
+console.log(angkaBoolean); // True karena 10 lebih besar dari 3.
+
+// NILAI KOSONG = keduanya digunakan untuk menunjukan ketiadaan nilai (the absence of somthing);
+// Null 
+// Undefined
