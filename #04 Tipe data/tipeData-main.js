024bc22b6a169164d8992fa616c1c1edcff2eed9
mhaemnn <muhaemin1998@gmail.com>
2025-10-09 22:43:52 +0700

    menambahkan folder vaiable

--- #04 Tipe data/tipeData-main.js
+++ #04 Tipe data/tipeData-main.js
@@ -1,6 +1,8 @@
 // Tipe data adalah element dasar yang sering kita disimpan dan olah untuk membuat insturksi.
 // Tipe data primitif => string, number, boolean, dan kosong (Null dan Undefined)
 
+console.log("\n===== TIPE DATA JAVASCRIPT ======");
+console.log
 // STRING
 let tipeData = "Ini adalah contoh tipe data string";
 console.log(tipeData);
@@ -31,14 +33,33 @@ let menu;
 console.log(menu);
 
 
-console.log("===== Mengubah Nilai Anter Tipe Data ======");
-// Mengubah tipe data ke string
+console.log("\n===== Mengubah Nilai Anter Tipe Data ======");
+// MENGUBAH TIPE DATA KE STRING
 // 1. menggunakan fungsi String()
 const number = 124;
 const strNumber = String(number);
-console.log(typeof(strNumber));
+console.log(`${strNumber} adalah bertipe ${typeof(strNumber)}`);
 
-// 2. menggunakan Method to.String()
-const boolean1 = false;
-const strBoolean1 = toString();
-console.log(typeof(strBoolean1));
+// 2. menggunakan Method .toString()
+const boolean1 = true;
+const strBoolean1 = boolean1.toString();
+// console.log(strBoolean1)
+console.log(`${strBoolean1} adalah bertipe ${typeof(strBoolean1)}`);
+
+// MENGUBAH STRING KE NUMBER
+// Menggunakan Number()
+const strNumber1 = "123";
+const strFloat = "23.4";
+
+const numFromString = Number(strNumber1);
+const flotFromSting = Number(strFloat);
+
+console.log(`${numFromString} adalah bertipe ${typeof{numFromString}}`);
+console.log(`${flotFromSting} adalah bertipe ${typeof{flotFromSting}}`);
+
+// Menggunakan parseInt() = membaca karekter satu persatu (20cm, 64bit);
+const cm = "20cm";
+const bit = "64bit";
+
+const intCm = parseInt(cm);
+const intBit = parseInt(bit);
